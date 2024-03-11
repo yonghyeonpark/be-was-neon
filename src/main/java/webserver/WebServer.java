59033914aa9b193ae3a95c126db7dd2eb2a863bd
yonghyeonpark <pyh4616@naver.com>
2024-03-12 01:38:35 +0900

    refactor: Concurrent 패키지 적용
    ExecutorService를 사용하여 스레드 풀을 생성했습니다.
    풀내의 스레드들을 통해 비동기적으로 실행됩니다.

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -1,10 +1,13 @@
 package webserver;
 
-import java.net.ServerSocket;
-import java.net.Socket;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class WebServer {
     private static final Logger logger = LoggerFactory.getLogger(WebServer.class);
@@ -18,6 +21,9 @@ public class WebServer {
             port = Integer.parseInt(args[0]);
         }
 
+        // 스레드 풀 생성
+        ExecutorService pool = Executors.newFixedThreadPool(8);
+
         // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
         try (ServerSocket listenSocket = new ServerSocket(port)) {
             logger.info("Web Application Server started {} port.", port);
@@ -25,9 +31,10 @@ public class WebServer {
             // 클라이언트가 연결될때까지 대기한다.
             Socket connection;
             while ((connection = listenSocket.accept()) != null) {
-                Thread thread = new Thread(new RequestHandler(connection));
-                thread.start();
+                pool.submit(new RequestHandler(connection));
             }
+        } catch (IOException e) {
+            pool.shutdown();
         }
     }
 }
