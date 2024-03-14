96d46e97083823de26270a7ce981c483eba4bac3
yonghyeonpark <pyh4616@naver.com>
2024-03-15 01:14:35 +0900

    refactor: 테스트 설명 피드백 반영
    테스트 설명을 조금 더 명확하게 작성했습니다.

--- src/test/java/webserver/HttpRequestTest.java
+++ src/test/java/webserver/HttpRequestTest.java
@@ -1,11 +1,12 @@
 package webserver;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class HttpRequestTest {
 
@@ -17,20 +18,20 @@ public class HttpRequestTest {
     }
 
     @Test
-    @DisplayName("request line에서 요청 target을 얻습니다.")
+    @DisplayName("request line을 공백 기준으로 나누어, 두 번째(인덱스1)에 해당하는 요청 target을 얻습니다.")
     void getRequestTarget() {
         String requestLine = "GET /registration/index.html HTTP/1.1";
-        Assertions.assertThat(httpRequest.getTarget(requestLine)).isEqualTo("/registration/index.html");
+        assertThat(httpRequest.getTarget(requestLine)).isEqualTo("/registration/index.html");
     }
 
     @Test
-    @DisplayName("쿼리를 파싱한 결과를 얻습니다.")
+    @DisplayName("쿼리문을 \"&\" 기준으로 필드를 나누고 \"=\" 기준으로 key-value 형태로 나누어 파싱하고 그 결과를 Map에 담습니다.")
     void getQueryParsingResult() {
         String query = "userId=kkk12&name=park&password=1234&email=kkk12@naver.com";
         Map<String, String> parameters = httpRequest.parseQuery(query);
-        Assertions.assertThat(parameters.get("userId")).isEqualTo("kkk12");
-        Assertions.assertThat(parameters.get("name")).isEqualTo("park");
-        Assertions.assertThat(parameters.get("password")).isEqualTo("1234");
-        Assertions.assertThat(parameters.get("email")).isEqualTo("kkk12@naver.com");
+        assertThat(parameters.get("userId")).isEqualTo("kkk12");
+        assertThat(parameters.get("name")).isEqualTo("park");
+        assertThat(parameters.get("password")).isEqualTo("1234");
+        assertThat(parameters.get("email")).isEqualTo("kkk12@naver.com");
     }
 }
