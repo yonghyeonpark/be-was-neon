6a1fec845ce98c0b07bac23b70f375aaa888e52c
yonghyeonpark <pyh4616@naver.com>
2024-03-14 11:41:30 +0900

    refactor: HttpRequest 수정
    내장 메서드들을 하나의 public 메서드에서 처리하던 구조에서, 각 내장
    메서드들을 public으로 선언하는 구조로 변경했습니다.

--- src/main/java/webserver/HttpRequest.java
+++ src/main/java/webserver/HttpRequest.java
@@ -5,63 +5,31 @@ import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.*;
-import java.nio.charset.StandardCharsets;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class HttpRequest {
 
     private static final String DEFAULT_PATH = "./src/main/resources/static";
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
+    private final List<String> headerLines;
 
-    private byte[] file;
-
-    public void processRequest(InputStream in) {
-        try {
-            BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
-
-            // 사용자 요청 메시지의 request line 읽어들임
-            String line = br.readLine();
-            logger.debug("request-line : " + line);
-            String url = getUrl(line);
-            logger.debug("url : " + url);
-
-            String[] split = url.split("\\?");
-            String target = split[0];
-            logger.debug("target : " + target);
-            // 요청 url에 쿼리문이 포함돼있을 때, 동작
-            if (split.length == 2) {
-                String query = split[1];
-                processQuery(query);
-            }
-            file = readFile(target);
-
-            while (!line.isEmpty()) {
-                line = br.readLine();
-                logger.debug("header-line : " + line);
-            }
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
+    public HttpRequest() {
+        this.headerLines = new ArrayList<>();
     }
 
-    private String getUrl(String line) {
+    public String getTarget(String line) {
         String[] requestLine = line.split(" ");
         return requestLine[1];
     }
 
-    private void processQuery(String query) {
-        Map<String, String> parameters = parseQuery(query);
-        User joinUser = new User(parameters.get("userId"),
-                parameters.get("password"),
-                parameters.get("name"),
-                parameters.get("email"));
-        Database.addUser(joinUser);
-        logger.debug("joinUser : " + joinUser);
-    }
-
-    private Map<String, String> parseQuery(String query) {
+    public Map<String, String> parseQuery(String query) {
         Map<String, String> parameters = new HashMap<>();
         if (query == null || query.isEmpty()) {
             return parameters;
@@ -74,20 +42,35 @@ public class HttpRequest {
         return parameters;
     }
 
-    private byte[] readFile(String target) {
-        File file = new File(DEFAULT_PATH + target);
+    public void processJoin(Map<String, String> parameters) {
+        User joinUser = new User(parameters.get("userId"),
+                parameters.get("password"),
+                parameters.get("name"),
+                parameters.get("email"));
+        Database.addUser(joinUser);
+        logger.debug("joinUser : " + joinUser);
+    }
+
+    public byte[] readFile(String path) {
+        File file = new File(DEFAULT_PATH + path);
         byte[] bytes = new byte[(int) file.length()];
         try (FileInputStream inputStream = new FileInputStream(file)) {
             inputStream.read(bytes);
         } catch (FileNotFoundException e) {
-            logger.error("target이 올바르지 않습니다.");
+            logger.error("path가 올바르지 않습니다.");
         } catch (IOException e) {
             e.printStackTrace();
         }
         return bytes;
     }
 
-    public byte[] getFile() {
-        return file;
+    public void addHeaderLine(String headerLine) {
+        headerLines.add(headerLine);
+    }
+
+    public void printHeaderLineLog() {
+        for (String headerLine : headerLines) {
+            logger.debug("[header-line] " + headerLine);
+        }
     }
 }
