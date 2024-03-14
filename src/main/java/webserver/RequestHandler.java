d32e8d0724bd4c3445c8b826dcf60acbd01726f8
yonghyeonpark <pyh4616@naver.com>
2024-03-14 11:45:41 +0900

    refactor: RequestHandler 수정
    HttpRequest에서 한 번에 처리하던 로직을, HttpRequest의 메서드를 사용하여 RequestHandler에서 처리하도록 수정

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,14 +3,12 @@ package webserver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class RequestHandler implements Runnable {
 
-    private static final String DEFAULT_PATH = "./src/main/resources/static";
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
 
     private Socket connection;
@@ -23,12 +21,32 @@ public class RequestHandler implements Runnable {
         logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+
+            // 사용자 요청 메시지의 request line을 읽어들임
+            String line = br.readLine();
+            logger.debug("request Line : " + line);
             HttpRequest httpRequest = new HttpRequest();
-            httpRequest.processRequest(in);
-            byte[] bytes = httpRequest.getFile();
+            String target = httpRequest.getTarget(line);
+            String[] splitTarget = target.split("\\?");
+            String path = splitTarget[0];
+            if (splitTarget.length == 2) {
+                String query = splitTarget[1];
+                httpRequest.processJoin(httpRequest.parseQuery(query));
+            }
+            byte[] file = httpRequest.readFile(path);
+
+            while (true) {
+                line = br.readLine();
+                if (line.isEmpty()) {
+                    break;
+                }
+                httpRequest.addHeaderLine(line);
+            }
+            httpRequest.printHeaderLineLog();
 
             HttpResponse httpResponse = new HttpResponse();
-            httpResponse.processResponse(out, bytes);
+            httpResponse.processResponse(out, file);
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
