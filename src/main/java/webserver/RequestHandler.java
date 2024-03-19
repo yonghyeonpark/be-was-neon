8a165df4943b25a2fd8a3b38d76f8a931de06c50
yonghyeonpark <pyh4616@naver.com>
2024-03-19 15:43:55 +0900

    refactor: 변경 사항 반영
    HttpRequest에서 headers를 가져와서 필요한 값을 꺼내 사용합니다.
    post로 들어온 회원가입 요청에 대해 302 응답을 하는 분기를 추가합니다.

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,6 +2,7 @@ package webserver;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import service.QueryProcessor;
 import webserver.httprequest.HttpRequest;
 
 import java.io.*;
@@ -26,42 +27,29 @@ public class RequestHandler implements Runnable {
         try (InputStream in = connection.getInputStream();
              OutputStream out = connection.getOutputStream()) {
             BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
-            
+
             HttpRequest httpRequest = new HttpRequest();
             httpRequest.readStartLine(br);
 
             // 요청 헤더 처리
             httpRequest.readHeaderLines(br);
             Map<String, String> headers = httpRequest.getHeaders();
-            logger.debug("content-length:{}", headers.get("Content-Length"));
             httpRequest.printHeaderLinesLog();
 
-            String method = httpRequest.getMethod();
-            logger.debug("method:{}", method);
+            HttpResponse httpResponse = new HttpResponse();
+            DataOutputStream dos = new DataOutputStream(out);
             String target = httpRequest.getTarget();
-            logger.debug("target:{}", target);
-
-            if (method.equalsIgnoreCase("POST")) {
-                String path = target;
-                logger.debug("path:{}", path);
-                logger.debug("Content-Length:{}", Integer.parseInt(headers.get("Content-Length")));
-                httpRequest.getBody(br, Integer.parseInt(headers.get("Content-Length")));
+            // 회원가입 요청에 대한 처리
+            if (httpRequest.getMethod().equalsIgnoreCase("POST") && target.equals("/user")) {
+                String body = httpRequest.getBody(br, Integer.parseInt(headers.get("Content-Length")));
+                Map<String, String> parameters = httpRequest.parseQuery(body);
+                QueryProcessor.userJoin(parameters);
+                httpResponse.response302Header(dos, "/index.html");
             }
 
-            /*if (httpRequest.isExistQuery(target)) {
-                String query = target[1];
-                Map<String, String> parameters = httpRequest.parseQuery(query);
-                logger.debug("parameters:{}", parameters);
-                QueryProcessor.userJoin(parameters);
-                logger.debug("body:{}", httpRequest.getBody(br));
-            }*/
-
             String contentType = httpRequest.getContentType(target);
-            DataOutputStream dos = new DataOutputStream(out);
             byte[] file = httpRequest.readFile(DEFAULT_PATH + target);
-
             // 해당 경로에 파일이 존재하지 않을 때
-            HttpResponse httpResponse = new HttpResponse();
             if (file == null) {
                 httpResponse.response404Header(dos, contentType);
                 httpResponse.responseBody(dos);
