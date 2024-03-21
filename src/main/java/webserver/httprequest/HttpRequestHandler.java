e7dc255d98e5ba82223d88c099caed3998e1eb93
yonghyeonpark <pyh4616@naver.com>
2024-03-21 18:51:28 +0900

    refactor: post 처리 로직 반환 타입 변경
    - processPost()의 반환 타입을 HttpResponse로 변경했습니다.
      - 요청 uri에 따라 필요한 정보를 HttpResponse의 생성자에 담아 객체를
        생성하여 반환합니다.

--- src/main/java/webserver/httprequest/HttpRequestHandler.java
+++ src/main/java/webserver/httprequest/HttpRequestHandler.java
@@ -3,7 +3,9 @@ package webserver.httprequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import service.QueryManager;
+import session.SessionManager;
 import webserver.MainHandler;
+import webserver.httpresponse.HttpResponse;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -15,25 +17,23 @@ public class HttpRequestHandler {
 
     private static final Logger logger = LoggerFactory.getLogger(MainHandler.class);
 
-    public static HttpRequest processUri(HttpRequest httpRequest) {
+    public static HttpResponse processUri(HttpRequest httpRequest) {
         final HttpRequest received = httpRequest;
         if (received.isPost()) {
             return processPost(received);
         }
         if (received.isGet()) {
-
+            // 파일 존재 확인
         }
         return null;
-
     }
-
-    private static HttpRequest processPost(HttpRequest received) {
+    
+    private static HttpResponse processPost(HttpRequest received) {
         String body = received.getBody();
         if (received.isMatchUri("/user/create")) {
             Map<String, String> parameters = QueryManager.parseQuery(body);
             QueryManager.userJoin(parameters);
-            received.setLocation("/index.html");
-            return received;
+            return new HttpResponse("HTTP/1.1 302 Found", "/index.html");
         }
 
         if (received.isMatchUri("/user/login")) {
@@ -41,11 +41,11 @@ public class HttpRequestHandler {
             // 로그인 성공
             if (QueryManager.checkLogin(parameters)) {
                 received.setLocation("/index.html");
-                return received;
+                return new HttpResponse("HTTP/1.1 302 Found", "/index.html", SessionManager.generateSessionId());
             }
             // 로그인 실패
             received.setLocation("/login/failed.html");
-            return received;
+            return new HttpResponse("HTTP/1.1 302 Found", "/login/failed.html");
         }
         return null;
     }
