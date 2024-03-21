b1b6571e67ee0116955f862b1ad5f5b7e2adc8d6
yonghyeonpark <pyh4616@naver.com>
2024-03-22 04:11:00 +0900

    refactor: run() 메서드 수정
    - 모든 로직이 진행되던 기존의 코드를 수정했습니다.
      - HttpRequestProcessor를 통해 요청 메시지의 정보들을 HttpRequest에 담고, 해당 정보들을 바탕으로 HttpRequestHandler를 통해 핵심 로직을 처리한 후, HttpResponse 객체를 반환합니다.
      - 반환된 HttpResponse 객체를 이용해서 HttpResponseProcessor 객체를
        생성하고 응답을 보냅니다.

--- src/main/java/webserver/MainHandler.java
+++ src/main/java/webserver/MainHandler.java
@@ -6,19 +6,19 @@ import webserver.httprequest.HttpRequest;
 import webserver.httprequest.HttpRequestHandler;
 import webserver.httprequest.HttpRequestProcessor;
 import webserver.httpresponse.HttpResponse;
+import webserver.httpresponse.HttpResponseProcessor;
 
 import java.io.*;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 
-public class RequestHandler implements Runnable {
+public class MainHandler implements Runnable {
 
-    private static final String DEFAULT_PATH = "./src/main/resources/static";
-    private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
+    private static final Logger logger = LoggerFactory.getLogger(MainHandler.class);
 
     private final Socket connection;
 
-    public RequestHandler(Socket connection) {
+    public MainHandler(Socket connection) {
         this.connection = connection;
     }
 
@@ -28,57 +28,13 @@ public class RequestHandler implements Runnable {
         try (InputStream in = connection.getInputStream();
              OutputStream out = connection.getOutputStream()) {
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
-
-            // 새로운 코드ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
             HttpRequest httpRequest = new HttpRequest(new HttpRequestProcessor(bufferedReader));
             httpRequest.printHeaderLinesLog();
 
-            HttpRequest processedRequest = HttpRequestHandler.uriProcess(httpRequest);
-            //ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
-
-
-            /*// 요청 헤더 처리
-            httpRequest.readHeaderLines(bufferedReader);
-            Map<String, String> headers = httpRequest.getHeaders();
-            httpRequest.printHeaderLinesLog();
-            String target = httpRequest.getTarget();*/
-
+            HttpResponse httpResponse = HttpRequestHandler.getUriProcessResult(httpRequest);
             DataOutputStream dos = new DataOutputStream(out);
-            HttpResponse httpResponse = new HttpResponse(dos);
-
-             // post 요청에 대한 처리
-            /*if (httpRequest.getMethod().equalsIgnoreCase("POST")) {
-                // 회원 가입 처리
-                if (target.equals("/user/create")) {
-                    String body = httpRequest.getBody(bufferedReader, Integer.parseInt(headers.get("Content-Length")));
-                    Map<String, String> parameters = httpRequest.parseQuery(body);
-                    QueryManager.userJoin(parameters);
-                    httpResponse.send302Response("/index.html");
-                    return;
-                }
-                // 로그인 처리
-                if (target.equals("/user/login")) {
-                    String body = httpRequest.getBody(bufferedReader, Integer.parseInt(headers.get("Content-Length")));
-                    Map<String, String> parameters = httpRequest.parseQuery(body);
-
-                    if (QueryManager.checkLogin(parameters)) {
-                        // 성공 페이지 리다이렉션
-                        httpResponse.send302Response("/index.html", SessionManager.generateSessionId());
-                        return;
-                    }
-                    // 실패 페이지로 리다이렉션
-                    httpResponse.send302Response("/login/failed.html");
-                }
-            }*/
-
-            String contentType = httpRequest.getContentType(target);
-            byte[] file = httpResponse.readFile(DEFAULT_PATH + target);
-            // 해당 경로에 파일이 존재하지 않을 때
-            if (file == null) {
-                httpResponse.send404Response(contentType);
-                return;
-            }
-            httpResponse.send200Response(file, contentType);
+            HttpResponseProcessor httpResponseProcessor = new HttpResponseProcessor(dos, httpResponse);
+            httpResponseProcessor.sendResponse();
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
